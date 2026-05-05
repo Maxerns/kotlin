8781f8bf89c7178c73bbfd7d56ddc2cf9e6278ca
Maksim Verevkin <maksim.verevkin@jetbrains.com>
2026-05-05 15:02:29 +0300

    JVM: introduce @JvmSpecialize annotation

--- compiler/fir/checkers/checkers-component-generator/src/org/jetbrains/kotlin/fir/checkers/generator/diagnostics/FirJvmDiagnosticsList.kt
+++ compiler/fir/checkers/checkers-component-generator/src/org/jetbrains/kotlin/fir/checkers/generator/diagnostics/FirJvmDiagnosticsList.kt
@@ -80,6 +80,8 @@ object JVM_DIAGNOSTICS_LIST : DiagnosticList("FirJvmErrors") {
         }
 
         val CONFLICT_VERSION_AND_JVM_OVERLOADS_ANNOTATION by warning<PsiElement>()
+
+        val JVM_SPECIALIZE_NOT_SUPPORTED_HERE by error<PsiElement>()
     }
 
     val TYPES by object : DiagnosticGroup("Types") {
