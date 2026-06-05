6bb44d655f898d74ee1ae05362cfc8b912b26966
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-06-05 10:45:15 +0200

    [build] Upgrade to Gradle 9.6
    - Replace delegate properties according to the update instructions:
        https://docs.gradle.org/9.6.0-rc-1/userguide/upgrading_version_9.html#changes_9.6.0

--- native/native.tests/klib-ir-inliner/build.gradle.kts
+++ native/native.tests/klib-ir-inliner/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("test-inputs-check-v2")
 }
 
-val llvmDevBinaryDataUsage by configurations.creating {
+val llvmDevBinaryDataUsage = configurations.create("llvmDevBinaryDataUsage") {
     isCanBeConsumed = false
     isCanBeResolved = true
 }
