a6bdfced58346b66fdab1dff189d470929529a88
Matej Tomášek <matej.tomasek@jetbrains.com>
2026-06-22 11:29:26 +0200

    Experimental implementation matching the compiler plugin

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
@@ -181,6 +181,7 @@ object CommonDeclarationCheckers : DeclarationCheckers() {
         FirTopLevelPropertiesChecker,
         FirPackageConflictsWithClassifierChecker,
         PlatformClassMappedToKotlinImportsChecker,
+        FirStaticInitializationChecker,
     )
 
     override val scriptCheckers: Set<FirScriptChecker> = setOf(
@@ -217,7 +218,7 @@ object CommonDeclarationCheckers : DeclarationCheckers() {
     )
 
     override val anonymousInitializerCheckers: Set<FirAnonymousInitializerChecker> = setOf(
-        FirAnonymousInitializerInInterfaceChecker
+        FirAnonymousInitializerInInterfaceChecker,
     )
 
     override val valueParameterCheckers: Set<FirValueParameterChecker> = setOf(
