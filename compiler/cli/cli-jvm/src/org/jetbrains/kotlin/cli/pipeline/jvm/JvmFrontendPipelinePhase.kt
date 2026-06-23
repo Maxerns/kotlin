d6f8466dee6217d575eaccb8e200b9c905176971
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-06-23 16:14:20 +0200

    ~ enable by default

--- compiler/cli/cli-jvm/src/org/jetbrains/kotlin/cli/pipeline/jvm/JvmFrontendPipelinePhase.kt
+++ compiler/cli/cli-jvm/src/org/jetbrains/kotlin/cli/pipeline/jvm/JvmFrontendPipelinePhase.kt
@@ -361,7 +361,7 @@ object JvmFrontendPipelinePhase : PipelinePhase<ConfigurationPipelineArtifact, J
         var firJvmIncrementalCompilationSymbolProvidersIsInitialized = false
 
         val javaDirectFacade =
-            if (configuration.languageVersionSettings.getFlag(JvmAnalysisFlags.useJavaDirect)) {
+            if (true /*configuration.languageVersionSettings.getFlag(JvmAnalysisFlags.useJavaDirect)*/) {
                 createJavaDirectSourceJavaFacadeBuilder(configuration, projectEnvironment)
             } else AbstractProjectEnvironment::getFirJavaFacade
         val context = FirJvmSessionFactory.Context(
