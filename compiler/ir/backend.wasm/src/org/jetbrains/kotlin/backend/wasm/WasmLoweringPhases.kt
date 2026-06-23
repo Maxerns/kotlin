c0ba13ef985dc0799f89d2c35d5ba1714896d3b1
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-06-23 17:05:23 +0200

    fixup! [IR] Replace `IrInterpreter` with `IrExpressionEvaluator`

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
@@ -101,7 +101,7 @@ private fun createObjectDeclarationLoweringPhase(context: JsCommonBackendContext
 
 //@PhasePrerequisites(FunctionInlining::class) // This prerequisite is hard to represent for common lowering
 private fun createConstEvaluationPhase(context: CommonBackendContext): ConstEvaluationLowering {
-    return ConstEvaluationLowering(context, platform = WasmPlatforms.unspecifiedWasmPlatform)
+    return ConstEvaluationLowering(context, isFloatingPointOptimizationDisabled = true)
 }
 
 fun wasmLoweringsOfTheFirstPhase(
--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/phases/NativeLoweringPhases.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/phases/NativeLoweringPhases.kt
@@ -602,7 +602,7 @@ internal val redundantCastsRemoverPhase = createFileLoweringPhase(
 
 internal val constEvaluationPhase = createFileLoweringPhase(
         lowering = { context: Context ->
-            ConstEvaluationLowering(context, platform = NativePlatforms.unspecifiedNativePlatform)
+            ConstEvaluationLowering(context, isFloatingPointOptimizationDisabled = false)
         },
         name = "ConstEvaluationLowering",
         prerequisite = setOf(inlineAllFunctionsPhase)
