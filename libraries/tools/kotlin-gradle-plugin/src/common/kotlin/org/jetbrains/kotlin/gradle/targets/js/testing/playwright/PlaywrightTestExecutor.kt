6ec9be4e47e001bc837032ae614076b20743b364
Nataliya.Valtman <nataliya.valtman@jetbrains.com>
2026-06-25 17:03:09 +0200

    Add task to install playwright browsers
    Directory to install playwright browsers could be set
    - via `PLAYWRIGHT_BROWSERS_PATH` environment variable
    - via `kotlin.gradle.playwright.browsers.path` Gradle property
    - or playwright default cache directory will be used
    
    ^KT-86449

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/playwright/PlaywrightTestExecutor.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/playwright/PlaywrightTestExecutor.kt
@@ -22,6 +22,7 @@ import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.nio.file.Path
+import kotlin.io.path.absolutePathString
 import kotlin.time.Duration
 
 private val log = LoggerFactory.getLogger("org.jetbrains.kotlin.gradle.tasks.testing.PlaywrightTestExecutor")
@@ -41,6 +42,7 @@ internal enum class PwBrowserKind {
 internal class PwRunnerSpec(
     val name: String,
     val browserKind: PwBrowserKind,
+    val browsersDirectory: Path,
     val testsLocation: KotlinJsTestsLocation,
     val buildTestsExecutionerUrl: (baseUrl: String) -> String,
     val timeout: Duration,
@@ -78,9 +80,12 @@ internal class PlaywrightTestExecutor() : TestExecuter<PwExecutionSpec> {
         handler.use {
             //  Use thin layer of Java Classes to interact with Playwright via std in/out pipes.
             val playwright = spec.createImpl(
-                Playwright.CreateOptions().setEnv(mapOf(
-                    "PLAYWRIGHT_NODEJS_PATH" to spec.nodeExecutable,
-                ))
+                Playwright.CreateOptions().setEnv(
+                    mapOf(
+                        "PLAYWRIGHT_NODEJS_PATH" to spec.nodeExecutable,
+                        "PLAYWRIGHT_BROWSERS_PATH" to spec.runners.first().browsersDirectory.absolutePathString()
+                    )
+                )
             )
 
             playwright.use {
@@ -163,7 +168,7 @@ internal class PlaywrightTestExecutor() : TestExecuter<PwExecutionSpec> {
     }
 
 
-    private fun PwExecutionSpec.createProcessBuilder( env: MutableMap<String, String>): ProcessBuilder {
+    private fun PwExecutionSpec.createProcessBuilder(env: MutableMap<String, String>): ProcessBuilder {
         val pb = ProcessBuilder(nodeExecutable)
         pb.command().add(playwrightCli) // This is the patched part. Original code loads playwright-cli from the JAR.
         pb.environment().putAll(env)
