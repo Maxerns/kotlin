93c3ec1f5a307bdcb0a61cc6c59e87855e506098
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2026-06-26 16:55:50 +0200

    fixup! [Kapt/Maven] support new option `kapt.stub.generation.scheme`
    Use enum for StubGenerationScheme option

--- libraries/tools/kotlin-maven-plugin/src/main/java/org/jetbrains/kotlin/maven/kapt/KaptJVMCompilerMojo.java
+++ libraries/tools/kotlin-maven-plugin/src/main/java/org/jetbrains/kotlin/maven/kapt/KaptJVMCompilerMojo.java
@@ -65,13 +65,19 @@ public class KaptJVMCompilerMojo extends K2JVMCompileMojo {
     @Parameter
     private boolean mapDiagnosticLocations = false;
 
+    private enum StubGenerationScheme {
+        // NOTE: these values are intentionally lowercase
+        jtree,
+        direct,
+    }
+
     /**
      * Selects how kapt generates Java stubs.
      * {@code jtree} is the historical scheme that uses javac AST and will be deprecated eventually.
      * {@code direct} is a new experimental scheme that generates Java text directly.
      */
     @Parameter(property = "kapt.stub.generation.scheme", defaultValue = "jtree")
-    private String stubGenerationScheme;
+    private StubGenerationScheme stubGenerationScheme;
 
     @Parameter
     private List<String> annotationProcessorArgs;
@@ -124,7 +130,7 @@ public class KaptJVMCompilerMojo extends K2JVMCompileMojo {
         options.add(new KaptOption("useLightAnalysis", useLightAnalysis));
         options.add(new KaptOption("correctErrorTypes", correctErrorTypes));
         options.add(new KaptOption("mapDiagnosticLocations", mapDiagnosticLocations));
-        options.add(new KaptOption("stubGenerationScheme", stubGenerationScheme));
+        options.add(new KaptOption("stubGenerationScheme", stubGenerationScheme.name()));
         options.add(new KaptOption("processors", annotationProcessors));
 
         if (includeCompileClasspath != null) {
