4fda6e8b18ec35c5dc1e1d56884a3c8295253b7f
Maksim Verevkin <maksim.verevkin@jetbrains.com>
2026-06-29 21:05:19 +0300

    JVM: move ReifiedOperationKind, ReificationArgument and some utils to util-jvm-inline-codegen

--- compiler/backend/src/org/jetbrains/kotlin/codegen/codegenUtil.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/codegenUtil.kt
@@ -6,9 +6,9 @@
 package org.jetbrains.kotlin.codegen
 
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
-import org.jetbrains.kotlin.codegen.inline.ReificationArgument
 import org.jetbrains.kotlin.codegen.inline.ReifiedTypeParametersUsages
 import org.jetbrains.kotlin.codegen.intrinsics.TypeIntrinsics
+import org.jetbrains.kotlin.codegen.util.inlinecodegen.ReificationArgument
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.util.isNullable
