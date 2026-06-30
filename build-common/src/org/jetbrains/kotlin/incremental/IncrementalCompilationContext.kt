e83f9870293da043431bc3839547b900214a07c2
Wojciech Kaliciński <wojciech.kalicinski@jetbrains.com>
2026-06-30 22:15:33 +0200

    [IC] Do not process synthetic files in RelocatableFileToPathConverter
    RelocatableFileToPathConverter normally throws on relative paths, but
    compiler plugins can generate callables that will
    be put in a synthetic source file with a relative path.
    We make use of a recorded list of such synthetic files to simply pass
    them as-is without attempting path conversions and without throwing
    errors.
    
    ^KT-87217 Verification pending
    ^RCA: There was no automated test for the scenario of compiler plugin generating top-level callables and JS incremental compilation. The change in commit 9f3b014 introduced more strict path checking, which was missed by tests. A regression test for this combination is added now.

--- build-common/src/org/jetbrains/kotlin/incremental/IncrementalCompilationContext.kt
+++ build-common/src/org/jetbrains/kotlin/incremental/IncrementalCompilationContext.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.incremental
 import com.intellij.util.io.KeyDescriptor
 import org.jetbrains.kotlin.build.report.DoNothingICReporter
 import org.jetbrains.kotlin.build.report.ICReporter
+import org.jetbrains.kotlin.compilerRunner.OutputItemsCollectorImpl
 import org.jetbrains.kotlin.incremental.storage.BasicFileToPathConverter
 import org.jetbrains.kotlin.incremental.storage.FileToPathConverter
 import java.io.File
@@ -31,7 +32,8 @@ class IncrementalCompilationContext(
     val trackLibrarySetChanges: Boolean = false,
     val icFeatures: IncrementalCompilationFeatures = IncrementalCompilationFeatures.DEFAULT_CONFIGURATION,
     val fragmentContext: FragmentContext? = null,
-    val useCompilerMapsOnly: Boolean = false
+    val useCompilerMapsOnly: Boolean = false,
+    val generatedSources: MutableSet<File> = mutableSetOf(),
 ) {
     @Deprecated("This constructor is scheduled to be removed. KSP is using it")
     constructor(
