e4542e0a0c07cb17c3b9ce7e13374d381b786af0
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-06-30 14:43:56 +0300

    [FIR] Add missing `when` branches to `collectSymbolsForType()`
    ^RCA:
    1. Root cause:
        1. I didn't think about it when I was implementing data-flow-based
           exhaustiveness. Most likely I incorrectly assumed leaving out
           some branches would likely keep red code red rather than break
           any green code.
    2. Preventive steps taken
        1. There's now a diagnostic test for it.
    3. Future steps
        1. All the missing branches have been added and the place now relies
           on the `when`'s exhaustiveness to prevent unexpected missing
           cases in the future.
    4. Additional notes
        1. No autotests, no related "unimportant" changes.
    
    ^KT-87253 Fixed

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/resolve/SupertypeUtils.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/resolve/SupertypeUtils.kt
@@ -60,7 +60,13 @@ fun collectSymbolsForType(type: ConeKotlinType, useSiteSession: FirSession): Lis
         when (val unwrappedType = unwrapToSimpleTypeUsingLowerBound().fullyExpandedType(useSiteSession)) {
             is ConeClassLikeType -> lookupTags.addIfNotNull(unwrappedType.lookupTag)
             is ConeIntersectionType -> unwrappedType.intersectedTypes.forEach { it.collectClassIds() }
-            else -> {}
+
+            is ConeCapturedType -> unwrappedType.constructor.supertypes?.forEach { it.collectClassIds() }
+            is ConeIntegerLiteralType -> {}
+            is ConeTypeParameterType -> unwrappedType.toTypeParameterSymbol(useSiteSession)?.resolvedBounds?.forEach { it.coneType.collectClassIds() }
+            is ConeLookupTagBasedType -> {}
+            is ConeStubType -> {}
+            is ConeTypeVariableType -> {}
         }
     }
 
--- compiler/testData/diagnostics/tests/missingCapturedTypeProcessingInEquals.kt
+++ compiler/testData/diagnostics/tests/missingCapturedTypeProcessingInEquals.kt
@@ -1,9 +1,9 @@
-// RUN_PIPELINE_TILL: FRONTEND
+// RUN_PIPELINE_TILL: BACKEND
 // ISSUE: KT-87253
 
 fun foo(vararg ts: String, a: Any?) {
     if (ts[0] == a) {
-        a.<!UNRESOLVED_REFERENCE!>length<!>
+        a.length
     }
 }
 
