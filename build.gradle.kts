8627448fd87c64678814019bc77a97c372a8cff5
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-07-01 11:49:23 +0300

    [Test] Delete `AbstractCompileKotlinAgainstKlibTest`
    There were two simple box test for JS backend, which is completely
    redundant due to our main codegen/box test set which is run for all
    klib-based backends.
    
    Also, delete the whole `:compiler:tests-against-klib` module.
    
    ^KTI-3382

--- build.gradle.kts
+++ build.gradle.kts
@@ -313,7 +313,6 @@ tasks {
             ":compiler:container:test",
             ":compiler:tests-java8:test",
             ":compiler:tests-spec:test",
-            ":compiler:tests-against-klib:test"
         )
     }
 
